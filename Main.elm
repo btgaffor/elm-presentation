1a734e7da68790945fb0deffe2f2af9b8789c247
Brandon Gafford <brandon.gafford@gmail.com>
2017-07-17 15:33:38 -0700

    tagged data demo

--- Main.elm
+++ Main.elm
@@ -1,51 +1,89 @@
-import Html exposing (Html, Attribute, program, div, span, input, text, h1, code, pre, br)
+import Html exposing (Html, Attribute, program, div, span, input, text, h1, code, pre, br, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 -- Model --
 
-type alias Model =
+type alias UserData =
   {
     firstName: String,
     lastName: String
   }
 
+type alias ContactData =
+  {
+    name: String,
+    phone: String
+  }
+
+type Model = User UserData
+           | Contact ContactData
+
 model : Model
 model =
-  {
-    firstName = "Joe",
-    lastName = "Schmoe"
-  }
+  User
+    {
+      firstName = "Joe",
+      lastName = "Schmoe"
+    }
 
 init: (Model, Cmd Action)
 init = (model, Cmd.none)
 
 -- Update --
 
-type Action = SetFirstName String
-            | SetLastName String
+type Action = SetFirstName UserData String
+            | SetLastName UserData String
+            | SetName ContactData String
+            | SetPhone ContactData String
+            | UseUser
+            | UseContact
 
 update : Action -> Model -> (Model, Cmd Action)
 update action model =
   case action of
-    SetFirstName newFirstName -> ({ model | firstName = newFirstName }, Cmd.none)
-    SetLastName newLastName -> ({ model | lastName = newLastName }, Cmd.none)
+    SetFirstName user newFirstName -> (User { user | firstName = newFirstName }, Cmd.none)
+    SetLastName user newLastName -> (User { user | lastName = newLastName }, Cmd.none)
+    SetName contact newName -> (Contact { contact | name = newName }, Cmd.none)
+    SetPhone contact newPhone -> (Contact { contact | phone = newPhone }, Cmd.none)
+    UseUser -> (User { firstName = "Joe", lastName = "Schmoe" }, Cmd.none)
+    UseContact -> (Contact { name = "Joe Schmoe", phone = "123-456-7890" }, Cmd.none)
 
 -- View --
 
 view : Model -> Html Action
 view model =
   div []
-    [ div []
-        [ input [ onInput SetFirstName, value model.firstName ] []
-        , input [ onInput SetLastName, value model.lastName ] []
-        ]
+    [ div [] (inputs model)
     , (name model)
     ]
 
+inputs : Model -> List (Html Action)
+inputs model =
+  case model of
+    User userData ->
+      [ input [ onInput (SetFirstName userData), value userData.firstName ] []
+      , input [ onInput (SetLastName userData), value userData.lastName ] []
+      ]
+    Contact contactData ->
+      [ input [ onInput (SetName contactData), value contactData.name ] []
+      , input [ onInput (SetPhone contactData), value contactData.phone ] []
+      ]
+
+
 name : Model -> Html Action
 name model =
-  span [] [ text (model.firstName ++ " " ++ model.lastName) ]
+  case model of
+    User { firstName, lastName } ->
+      div []
+        [ div [] [ text (firstName ++ " " ++ lastName) ]
+        , button [ onClick UseContact ] [ text "Use Contact" ]
+        ]
+    Contact { name, phone } ->
+      div []
+        [ div [] [ text (name ++ " (" ++ phone ++ ")" )]
+        , button [ onClick UseUser ] [ text "Use User" ]
+        ]
 
 -- Main --
 
