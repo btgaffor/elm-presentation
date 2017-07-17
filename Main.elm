849278c3067ffd691a596e6aae550476adcf1a79
Brandon Gafford <brandon.gafford@gmail.com>
2017-07-17 15:05:50 -0700

    add randomization demo

--- Main.elm
+++ Main.elm
@@ -1,6 +1,8 @@
-import Html exposing (Html, Attribute, program, div, span, input, text, h1, code, pre, br)
+import Html exposing (Html, Attribute, program, div, span, input, text, h1, code, pre, br, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Random
+import Array exposing (Array)
 
 -- Model --
 
@@ -24,12 +26,43 @@ init = (model, Cmd.none)
 
 type Action = SetFirstName String
             | SetLastName String
+            | RequestRandomize
+            | PerformRandomize Int
 
 update : Action -> Model -> (Model, Cmd Action)
 update action model =
   case action of
     SetFirstName newFirstName -> ({ model | firstName = newFirstName }, Cmd.none)
     SetLastName newLastName -> ({ model | lastName = newLastName }, Cmd.none)
+    RequestRandomize -> (model, Random.generate PerformRandomize (Random.int Random.minInt Random.maxInt))
+    PerformRandomize seed -> ((randomizeNames model seed), Cmd.none)
+
+randomizeNames : Model -> Int -> Model
+randomizeNames model seedRandomizer =
+  let
+    initialSeed = Random.initialSeed seedRandomizer
+    (newFirstName, firstNameSeed) = getFirstName initialSeed
+    (newLastName, lastNameSeed) = getLastName firstNameSeed
+  in
+    { model | firstName = newFirstName, lastName = newLastName }
+
+getFirstName : Random.Seed -> (String, Random.Seed)
+getFirstName initialSeed =
+  let
+    (index, newSeed) = Random.step (Random.int 0 3) initialSeed
+  in
+    case (Array.fromList ["James", "Mary", "John", "Elizabeth"] |> Array.get index) of
+      Just name -> (name, newSeed)
+      Nothing -> ("Joe", newSeed)
+
+getLastName : Random.Seed -> (String, Random.Seed)
+getLastName initialSeed =
+  let
+    (index, newSeed) = Random.step (Random.int 0 3) initialSeed
+  in
+    case (Array.fromList ["Smith", "Johnson", "Williams", "Jones"] |> Array.get index) of
+      Just name -> (name, newSeed)
+      Nothing -> ("Smith", newSeed)
 
 -- View --
 
@@ -41,11 +74,12 @@ view model =
         , input [ onInput SetLastName, value model.lastName ] []
         ]
     , (name model)
+    , button [ onClick RequestRandomize ] [ text "Randomize!" ]
     ]
 
 name : Model -> Html Action
 name model =
-  span [] [ text (model.firstName ++ " " ++ model.lastName) ]
+  div [] [ text (model.firstName ++ " " ++ model.lastName) ]
 
 -- Main --
 
